93c89cc20238c4806e062b7b5ead3ba0fc80f9b9
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-15 17:03:30 -0500

    adds range validation to user input

--- project1.py
+++ project1.py
@@ -16,11 +16,20 @@ print(row1[position_index])
 
 def user_choice():
     choice = "yes"
-    while choice.isdigit() == False:
+    acceptable_range = range(0, 10)
+    within_range = False
+    while choice.isdigit() == False or within_range == False:
         choice = input("Please enter a number (0-10): ")
 
         if choice.isdigit() == False:
             print("Sorry that is not a correct number")
+
+        if choice.isdigit() == True:
+            if int(choice) in acceptable_range:
+                within_range = True
+            else:
+                print("Sorry you are out of the acceptable range (0-10)")
+                within_range = False
 
     return int(choice)
 
