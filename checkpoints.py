a9cd24d20016d61a05101fc51571f343eb15d4bd
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-15 13:45:58 -0500

    adds count primes function

--- checkpoints.py
+++ checkpoints.py
@@ -11,3 +11,20 @@ def myfunc(str):
 
 
 print(myfunc("Anthromorphism"))
+
+
+def count_primes(num):
+    primes = [2]
+    x = 3
+    if num < 2:
+        return 0
+    while x <= num:
+        for y in primes:
+            if x % y == 0:
+                x += 2
+                break
+        else:
+            primes.append(x)
+            x += 2
+    print(primes)
+    return len(primes)
