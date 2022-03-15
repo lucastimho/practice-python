c6782f56e1d052cb8c4593ff97d9668621108eb8
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-15 14:08:24 -0500

    adds filter function

--- lambda.py
+++ lambda.py
@@ -5,3 +5,11 @@ def square(nums):
 my_nums = [1, 2, 3, 4, 5, 6]
 for items in map(square, my_nums):
     print(items)
+
+
+def check_even(num):
+    return num % 2 == 0
+
+
+for items in filter(check_even, my_nums):
+    print(items)
