31bae0a21f5f645848f39f0a0d80802fbb3305ce
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-16 13:54:47 -0500

    adds game on or off function

--- project1.py
+++ project1.py
@@ -60,3 +60,29 @@ def postion_choice():
 
 
 postion_choice()
+
+
+def replacement_choice(game_list, position):
+    user_placement = input("Type a string to place at position: ")
+    game_list[position] = user_placement
+    return game_list
+
+
+replacement_choice(game_list, 1)
+
+
+def gameon_choice():
+    choice = "wrong"
+    while choice not in ['Y', 'N']:
+        choice = input("Keep playing? (Y, N): ")
+
+        if choice not in ['0', '1', '2']:
+            print("Sorry I don't understand, please input Y or N ")
+
+    if choice == "Y":
+        return True
+    else:
+        return False
+
+
+gameon_choice()
