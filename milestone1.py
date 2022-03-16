8461092c7b62d3e9e59cb5069e4ca6df818a4310
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-16 15:19:26 -0500

    adds code to account for blank spaces in tic-tac-toe

--- milestone1.py
+++ milestone1.py
@@ -58,23 +58,23 @@ def player2_inputs():
 
 
 def win_state():
-    if board[0] == board[1] and board[0] == board[2]:
+    if board[0] == board[1] and board[0] == board[2] and board[0] != ' ':
         return True
-    elif board[0] == board[4] and board[0] == board[8]:
+    elif board[0] == board[4] and board[0] == board[8] and board[0] != ' ':
         return True
-    elif board[6] == board[4] and board[6] == board[2]:
+    elif board[6] == board[4] and board[6] == board[2] and board[6] != ' ':
         return True
-    elif board[3] == board[4] and board[3] == board[5]:
+    elif board[3] == board[4] and board[3] == board[5] and board[3] != ' ':
         return True
-    elif board[6] == board[7] and board[6] == board[8]:
+    elif board[6] == board[7] and board[6] == board[8] and board[6] != ' ':
         return True
-    elif board[0] == board[3] and board[0] == board[6]:
+    elif board[0] == board[3] and board[0] == board[6] and board[0] != ' ':
         return True
-    elif board[1] == board[4] and board[1] == board[7]:
+    elif board[1] == board[4] and board[1] == board[7] and board[1] != ' ':
         return True
-    elif board[2] == board[5] and board[2] == board[8]:
+    elif board[2] == board[5] and board[2] == board[8] and board[2] != ' ':
         return True
-    elif board[6] == board[7] and board[6] == board[8]:
+    elif board[6] == board[7] and board[6] == board[8] and board[6] != ' ':
         return True
     else:
         return False
