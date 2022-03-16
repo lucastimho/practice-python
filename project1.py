71cb2888d4581e461d0d6f751b58732825d1f321
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-16 13:34:16 -0500

    comments out beginning and begin interactice game

--- project1.py
+++ project1.py
@@ -1,37 +1,47 @@
-def display(a, b, c):
-    print(a)
-    print(b)
-    print(c)
+# def display(a, b, c):
+#     print(a)
+#     print(b)
+#     print(c)
 
 
-row1 = ['', '', '']
-row2 = ['', '', '']
-row3 = ['', '', '']
+# row1 = ['', '', '']
+# row2 = ['', '', '']
+# row3 = ['', '', '']
 
-display(row1, row2, row3)
+# display(row1, row2, row3)
 
-position_index = input("Choose an index position: ")
-print(row1[position_index])
-
-
-def user_choice():
-    choice = "yes"
-    acceptable_range = range(0, 10)
-    within_range = False
-    while choice.isdigit() == False or within_range == False:
-        choice = input("Please enter a number (0-10): ")
-
-        if choice.isdigit() == False:
-            print("Sorry that is not a correct number")
-
-        if choice.isdigit() == True:
-            if int(choice) in acceptable_range:
-                within_range = True
-            else:
-                print("Sorry you are out of the acceptable range (0-10)")
-                within_range = False
-
-    return int(choice)
+# position_index = input("Choose an index position: ")
+# print(row1[position_index])
 
 
-user_choice()
+# def user_choice():
+#     choice = "yes"
+#     acceptable_range = range(0, 10)
+#     within_range = False
+#     while choice.isdigit() == False or within_range == False:
+#         choice = input("Please enter a number (0-10): ")
+
+#         if choice.isdigit() == False:
+#             print("Sorry that is not a correct number")
+
+#         if choice.isdigit() == True:
+#             if int(choice) in acceptable_range:
+#                 within_range = True
+#             else:
+#                 print("Sorry you are out of the acceptable range (0-10)")
+#                 within_range = False
+
+#     return int(choice)
+
+
+# user_choice()
+
+game_list = [0, 1, 2]
+
+
+def display_game(game_list):
+    print("This is the current game list: ")
+    print(game_list)
+
+
+display_game(game_list)
