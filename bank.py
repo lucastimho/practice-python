da724731711b16d36fee08c9f98e6b14c41c2c6e
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-21 13:10:46 -0500

    adds withdraw method

--- bank.py
+++ bank.py
@@ -8,8 +8,17 @@ class Bank():
         self.balance = self.balance + deposit
         print("Balance: $" + str(self.balance))
 
+    def withdraw(self, withdraw):
+        if withdraw < self.balance:
+            self.balance = self.balance - withdraw
+            print("Your remaining balance is $" + str(self.balance) + ".")
+        else:
+            print("You do not have enough remaining balance to withdraw that amount.")
+
 
 jose = Bank("Jose", 400)
 print(jose.owner)
 print(jose.balance)
 jose.deposit(1200)
+jose.withdraw(200)
+jose.withdraw(5000)
