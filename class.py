fbc563697869d4fdcc6708917f40cd78a3a9938e
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-21 11:57:40 -0500

    adds Dog and Cat class for polymorphism

--- class.py
+++ class.py
@@ -9,7 +9,32 @@ class Animal():
         print("I am eating.")
 
 
-class Dog(Animal):
-    def __init__(self):
-        Animal.__init__(self)
-        print("Dog created.")
+# class Dog(Animal):
+#     def __init__(self):
+#         Animal.__init__(self)
+#         print("Dog created.")
+
+#     def who_i_am(self):
+#         print("I am a dog.")
+
+#     def bark(self):
+#         print("WOOF!")
+
+
+# my_dog = Dog()
+# print(my_dog)
+
+class Dog():
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return self.name + " says woof."
+
+
+class Cat():
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return self.name + " says meow."
