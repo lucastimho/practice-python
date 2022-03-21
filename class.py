0e892afa8e0e8ca20732bb81a43524d1aa36b9a4
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-21 12:10:25 -0500

    implements class polymorphism

--- class.py
+++ class.py
@@ -50,3 +50,31 @@ def pet_speak(pet):
 
 
 pet_speak(felix)
+
+
+class Animal():
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        raise NotImplementedError(
+            "Subclass must implement this abstract method")
+
+
+class Dog(Animal):
+
+    def speak(self):
+        return self.name + " says woof."
+
+
+class Cat(Animal):
+
+    def speak(self):
+        return self.name + " says meow."
+
+
+rick = Dog("Rick")
+sally = Cat("Sally")
+
+print(rick.speak())
+print(sally.speak())
