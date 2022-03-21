ed1cbc9405cf654cf53df05799dae83a5af7597b
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-21 13:03:55 -0500

    adds deposit method

--- bank.py
+++ bank.py
@@ -3,7 +3,13 @@ class Bank():
         self.owner = owner
         self.balance = balance
 
+    def deposit(self, deposit):
+        print("Owner: " + self.owner)
+        self.balance = self.balance + deposit
+        print("Balance: $" + str(self.balance))
+
 
 jose = Bank("Jose", 400)
 print(jose.owner)
 print(jose.balance)
+jose.deposit(1200)
