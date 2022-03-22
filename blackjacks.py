c31420109db4bd6b14b863021dfeb0217928b640
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-22 15:59:34 -0500

    creates Deck class

---
+++ blackjacks.py
@@ -0,0 +1,7 @@
+import random
+
+
+class Deck():
+    def __init__(self):
+        self.deck = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7,
+                     "eight": 8, "nine": 9, "ten": 10, "jack": 10, "queen": 10, "king": 10, "ace": 11}
