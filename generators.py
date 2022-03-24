1c965a37301176356cc7d05953281bea09c0499e
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-23 23:21:02 -0500

    creates fibonacci sequence using generators

--- generators.py
+++ generators.py
@@ -5,3 +5,15 @@ def create_cubes(n):
 
 
 print(list(create_cubes(10)))
+
+
+def gen_fibon(n):
+    a = 1
+    b = 1
+    for i in range(n):
+        yield a
+        a, b = b, a+b
+
+
+for number in gen_fibon(10):
+    print(number)
