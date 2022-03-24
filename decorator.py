3cfb4fc298d3d7ba591a3bc0e4f2edf39d3737c6
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-23 23:11:50 -0500

    sets up decorated function and allows for on and off switch

--- decorator.py
+++ decorator.py
@@ -61,3 +61,11 @@ func_needs_decorator()
 
 decorated_func = new_decorator(func_needs_decorator)
 decorated_func()
+
+
+@new_decorator
+def func_needs_decorator():
+    print("I want to be decorated!")
+
+
+func_needs_decorator()
