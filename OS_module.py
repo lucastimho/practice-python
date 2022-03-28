47f907609cc53b0cfe4b9bcc13b6950876351e40
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-28 16:52:29 -0500

    notes in shutil commands that deletes files and folders

--- OS_module.py
+++ OS_module.py
@@ -13,3 +13,7 @@ list = os.listdir(path)
 for p in list:
   print(p)
 # shutil.move('practice.txt', '/Users/lucasho/Acutalize')
+# shutil.unlink(path) deletes a file at the path I decide
+# shutil.rmdir(path) deletes an empty folder
+# shutil.rmtree(path) removes all files and folers (dangerous)
+# could always pip install send2trash
