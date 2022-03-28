fb90a8254f6a33866b5b81c85ab13f2ea770abc7
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-28 18:01:55 -0500

    adds more math function

--- math.py
+++ math.py
@@ -1,4 +1,12 @@
 import math
+
 value = 4.5
 math.floor(value)
 math.ceil(value)
+round(value)
+print(math.pi)
+print(math.e)
+math.log(100, 10)
+math.sin(10)
+math.degrees(math.pi/2)
+math.radians(180)
