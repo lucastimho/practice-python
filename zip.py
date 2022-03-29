3036ccbd9cff5bf63911fc38565ae723bc0d61c9
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-29 14:55:21 -0500

    uses shutil modules and the make_archive and upack_archives methods

--- zip.py
+++ zip.py
@@ -7,6 +7,7 @@ f.write("TWO FILE")
 f.close()
 
 import zipfile
+import os
 
 comp_file = zipfile.ZipFile("comp_file.zip", "w")
 comp_file.write("fileone.txt", compress_type=zipfile.ZIP_DEFLATED)
@@ -14,3 +15,10 @@ comp_file.write("filetwo.txt", compress_type=zipfile.ZIP_DEFLATED)
 comp_file.close()
 zip_obj = zipfile.ZipFile("comp_file.zip", "r")
 zip_obj.extractall("extracted_content")
+
+import shutil
+
+dir_to_zip = str(os.getcwd()) + "/extracted_content"
+output_filename = 'example'
+shutil.make_archive(output_filename, "zip", dir_to_zip)
+shutil.unpack_archive("example.zip", "final_zip", "zip")
