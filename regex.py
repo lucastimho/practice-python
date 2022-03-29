91b6bcd8010ff722e99b3ba67cd8f3f201d29974
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-29 13:06:10 -0500

    uses the re module search, findall, and finditer methods

---
+++ regex.py
@@ -0,0 +1,19 @@
+text = "The agent's phone number is 408-555-1234. Call soon!!!"
+print("phone" in text)
+
+import re
+
+pattern = "phone"
+print(re.search(pattern, text))
+print(re.search("Hello", text))
+match = re.search(pattern, text)
+print(match.span())
+print(match.start())
+print(match.end())
+text = "one phone, two phones."
+match = re.search(pattern, text)
+print(match.span())
+matches = re.findall(pattern, text)
+print(matches)
+for match in re.finditer(pattern, text):
+  print(match.span())
