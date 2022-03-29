a61d7c573cfc438492c0505acf3a132d9d89867d
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-29 13:58:33 -0500

    uses advanced regex searches within strings

--- regex.py
+++ regex.py
@@ -25,3 +25,16 @@ phone_pattern = re.compile("(\d{3})-(\d{3})-(\d{4})")
 results = re.search(phone_pattern, text)
 print(results.group())
 print(results.group(2))
+search = re.search("cat|dog", "The dog is here.")
+print(search.span())
+search = re.findall("...at", "The cat in the hat sat went splat.")
+print(search)
+print(re.findall("^\d", "The 2 is a number."))
+phrase = "There 5 are numbers 34 inside this statement 5."
+pattern = "[^\d]+"
+print(re.findall(pattern, phrase))
+test_phrase = "This is a string! But is has punctuation. How can we remove it?"
+print(" ".join(re.findall("[^!.?]+", test_phrase)))
+text = "Only find the hyphen-words in this statement. But you do not know how long-ish they are."
+pattern = "[\w]+-[\w]+"
+print(re.findall(pattern, text))
