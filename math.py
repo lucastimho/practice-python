52edbac31fd9393dd05d29226a7d8ccbeece0f04
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-31 00:19:30 -0500

    comments out math.py in hopes to fix virual python env

--- math.py
+++ math.py
@@ -1,26 +1,26 @@
-import math
-import random
+# import math
+# import random
 
-value = 4.5
-math.floor(value)
-math.ceil(value)
-round(value)
-print(math.pi)
-print(math.e)
-math.log(100, 10)
-math.sin(10)
-math.degrees(math.pi/2)
-math.radians(180)
-random.seed(101)
-random.randint(0,100)
-mylist = list(range(0, 20))
-# sample with replacement
-random.choice(mylist)
-random.choices(population=mylist, k=10)
-# sample without replacement
-random.sample(population=mylist, k=10)
-random.shuffle(mylist)
-# uniform distribution
-random.uniform(a=0, b=100)
-# gaussian distribution
-random.gauss(mu=0, sigma=1)
+# value = 4.5
+# # math.floor(value)
+# # math.ceil(value)
+# round(value)
+# print(math.pi)
+# print(math.e)
+# math.log(100, 10)
+# math.sin(10)
+# math.degrees(math.pi/2)
+# math.radians(180)
+# random.seed(101)
+# random.randint(0, 100)
+# mylist = list(range(0, 20))
+# # sample with replacement
+# random.choice(mylist)
+# random.choices(population=mylist, k=10)
+# # sample without replacement
+# random.sample(population=mylist, k=10)
+# random.shuffle(mylist)
+# # uniform distribution
+# random.uniform(a=0, b=100)
+# # gaussian distribution
+# random.gauss(mu=0, sigma=1)
